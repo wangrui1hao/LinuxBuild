82bf8057e7aa13589629c21f73b88e4c38f01049
ruiwang <49175615+wangrui1hao@users.noreply.github.com>
2023-10-19 02:14:48 +0800

    Update build_linux_not_root.sh

--- build_linux_not_root.sh
+++ build_linux_not_root.sh
@@ -12,26 +12,22 @@ not_root_need
 #安装go相关
 cd ~/
 go get github.com/gogo/protobuf/protoc-gen-gofast
-export GO111MODULE=on
-go get -u github.com/golang/protobuf/protoc-gen-go@v1.3.2
-export GO111MODULE=auto
-go get -u github.com/mailru/easyjson/...
+go install github.com/golang/protobuf/protoc-gen-go@v1.3.2
+go install github.com/mailru/easyjson/...
 go get -u golang.org/x/lint/golint
 go get golang.org/x/tools/cmd/stringer
 go install golang.org/x/tools/gopls@latest
 go install github.com/go-delve/delve/cmd/dlv@latest
 curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $HOME/go/bin v1.54.2
 echo "export PATH=\$HOME/go/bin:\$PATH" | sudo tee -a /etc/profile
-sudo source /etc/profile
 
 #拉取nvim配置
 yum install -y git
-cd ~/
 if [ ! -d ~/.config ];then
     mkdir ~/.config
 fi 
 if [ -d ~/.config/nvim ];then 
-    rm -rf ./nvim
+    rm -rf ~/.config/nvim
 fi 
 
 cd ~/.config
