244e07a43454ccb22f10c55d41a184826fb8b352
rui.wang05 <rui.wang05@mihoyo.com>
2023-10-27 17:23:46 +0800

    update

---
+++ bashrc
@@ -0,0 +1,79 @@
+# .bashrc
+
+# Source global definitions
+if [ -f /etc/bashrc ]; then
+	. /etc/bashrc
+fi
+
+# Uncomment the following line if you don't like systemctl's auto-paging feature:
+# export SYSTEMD_PAGER=
+
+# User specific aliases and functions
+export PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH"
+export SCREENDIR=$HOME/.screen
+
+# golang环境相关
+export GOPATH="$HOME/go:$NXXCDC"
+export GO111MODULE=auto
+
+# 其他
+export GTAGSCONF=/usr/local/gtags/share/gtags/gtags.conf
+export GTAGSLABEL=pygments
+export SVN_EDITOR=vim
+
+# 端口映射
+function AddressPortMapping {
+	# 需要映射的端口ID加在这里
+	PORT_MAPPING=(
+		80
+		2223
+		3306
+		8500
+	)
+	LINUX_IP=`ifconfig | grep "inet " | grep -v "127.0.0.1" | awk '{print $2}'`
+	
+	for CURR_PORT in ${PORT_MAPPING[@]}
+	do
+		NEW_STRING="netsh interface portproxy add v4tov4 listenport="$CURR_PORT" listenaddress=0.0.0.0 connectport="$CURR_PORT" connectaddress="$LINUX_IP";"$NEW_STRING
+	done
+	# 此处的'pc'可在配置文件内替换为自己的windows地址
+	ssh pc $NEW_STRING > /dev/null
+}
+
+# 启动redis-server
+function RunRedis {
+	redis-server /data/Redis/conf/redis.conf
+}
+
+# 启动consul
+function RunConsul {
+	consul agent -server -bootstrap -client 0.0.0.0 -ui -data-dir=/data/Consul/data -config-dir=/data/Consul/conf > /data/Consul/logs/consul-log.log &
+}
+
+# 启动mysql
+function RunMysql {
+	#sudo mkdir -p /var/run/mysqld
+	#sudo chown mysql.mysql /var/run/mysqld
+	sudo systemctl restart mysqld
+}
+
+# 启动sshd
+function RunSshd {
+	sudo systemctl restart sshd
+}
+
+# 启动一键脚本
+function RestartRun {
+	AddressPortMapping > /dev/null &
+	RunRedis > /dev/null &
+	RunConsul > /dev/null &
+	screen -wipe > /dev/null &
+	SVNAgent > /dev/null &
+	wait
+}
+
+# 重启svn托管服务
+function SVNAgent {
+	pkill gpg-agent
+	gpg-agent --daemon
+}
--- build_linux_not_root.sh
+++ build_linux_not_root.sh
@@ -8,29 +8,58 @@ function not_root_need() {
 }
 
 not_root_need
+cd ~ && sudo chown -R $USER:$USER /data
 
-#安装go相关
-cd ~/
-go get github.com/gogo/protobuf/protoc-gen-gofast
-#go install github.com/golang/protobuf/protoc-gen-go@v1.3.2
-go install github.com/golang/protobuf/protoc-gen-go@latest
-go install github.com/mailru/easyjson/...@latest
-#go get -u golang.org/x/lint/golint
-go install golang.org/x/tools/cmd/stringer@latest
-go install golang.org/x/tools/gopls@latest
-go install github.com/go-delve/delve/cmd/dlv@latest
-curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $HOME/go/bin v1.55.0
-echo "export PATH=\$HOME/go/bin:\$PATH" | sudo tee -a /etc/profile
+# protoc-gen-gofast
+is_install=`ls $HOME/go/bin 2>&1 | grep protoc-gen-gofast`
+if [ -z "$is_install" ]; then
+    go get github.com/gogo/protobuf/protoc-gen-gofast
+fi
+
+# protoc-gen-go
+is_install=`ls $HOME/go/bin 2>&1 | grep protoc-gen-go`
+if [ -z "$is_install" ]; then
+    go install github.com/golang/protobuf/protoc-gen-go@latest
+fi
+
+# easyjson
+is_install=`ls $HOME/go/bin 2>&1 | grep easyjson`
+if [ -z "$is_install" ]; then
+    go install github.com/mailru/easyjson/...@latest
+fi
+
+
+# stringer
+is_install=`ls $HOME/go/bin 2>&1 | grep stringer`
+if [ -z "$is_install" ]; then
+    go install golang.org/x/tools/cmd/stringer@latest
+fi
+
+# gopls
+is_install=`ls $HOME/go/bin 2>&1 | grep gopls`
+if [ -z "$is_install" ]; then
+    go install golang.org/x/tools/gopls@latest
+fi
+
+# dlv
+is_install=`ls $HOME/go/bin 2>&1 | grep dlv`
+if [ -z "$is_install" ]; then
+    go install github.com/go-delve/delve/cmd/dlv@latest
+fi
+
+# golangci-lint
+is_install=`ls $HOME/go/bin 2>&1 | grep golangci-lint`
+if [ -z "$is_install" ]; then
+    curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $HOME/go/bin v1.55.0
+fi
+
+# gopath-bin
+is_install=`cat /etc/profile | grep "HOME/go/bin"`
+if [ -z "$is_install" ]; then
+    echo "export PATH=\$HOME/go/bin:\$PATH" | sudo tee -a /etc/profile
+    source /etc/profile
+fi
 
 #拉取nvim配置
-yum install -y git
-if [ ! -d ~/.config ];then
-    mkdir ~/.config
-fi 
-if [ -d ~/.config/nvim ];then 
-    rm -rf ~/.config/nvim
-fi 
-
-cd ~/.config
-
-git clone https://github.com/wangrui1hao/nvim-for-server.git nvim --depth=1 
+mkdir -p ~/.config && rm -rf ~/.config/nvim
+cd ~/.config && git clone https://github.com/wangrui1hao/nvim-for-server.git nvim --depth=1 
